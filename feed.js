a65b4e93f6dd065707fab44ed222c60215a96b5f
Rimo Esko <rimoesk@greeny.cs.tlu.ee>
2016-04-13 10:17:10 +0300

    added html and js

---
+++ feed.js
@@ -0,0 +1,21 @@
+$(function(){
+  //laetud
+
+  var content  = $("#content");
+
+  getTweets();
+});
+
+function getTweets(){
+  //ajax
+  $.ajax({
+    url: "getfeed.php",
+    success: function(data){
+      var array = JSON.parse(data).statuses;
+      console.log(array);
+    },
+    error: function(error){
+      console.log(error);
+    }
+  });
+}
