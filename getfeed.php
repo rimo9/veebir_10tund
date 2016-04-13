2de8c26a297f67f176a532bf7d09dad058852147
Rimo Esko <rimoesk@greeny.cs.tlu.ee>
2016-04-13 09:38:42 +0300

    adding values to array

--- getfeed.php
+++ getfeed.php
@@ -8,6 +8,16 @@
   $getField = "?q=%23Paris&result_type=recent";
   $requestMethod = "GET";
 
+  $file_data = json_decode(file_get_contents($file_name));//faili sisu tagasi objektiks
+  //võrdleme aega
+  $delay = 10;//10sekundit
+  //kas on möödunud vähem kui delayga määratud aeg
+  if((strtotime(date("c")) - strtotime($file_data->date)) < $delay){
+    //liiga vähe möödas
+    echo json_encode($file_data);
+    return;
+  }
+
   $twitter = new TwitterAPIExchange($config);
   $dataFromAPI = $twitter->setGetfield($getField)
                          ->buildOauth($url, $requestMethod)
@@ -17,6 +27,17 @@
   $object = new stdClass();
   $object->date = date("c");
   $object->statuses = json_decode($dataFromAPI)->statuses;
+
+  //lisan vanad mis jäänud teksti faili siia juurde
+  foreach($file_data->statuses as $old_status){
+    $exists = false;
+    foreach($object->statuses as $new_status){
+      //vaatame kas on olemas
+      if($old_status->id == $new_status->id){$exists = true;}
+    }
+    if(!$exists){array_push($object->statuses, $old_status);}
+  }
+  //echo count($object->statuses);
 
   file_put_contents($file_name, json_encode($object));
 
