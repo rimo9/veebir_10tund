45f5fc7acfd1191247e9113aef92df1c819b09ca
Rimo Esko <rimoesk@greeny.cs.tlu.ee>
2016-04-13 09:20:18 +0300

    added text file

--- getfeed.php
+++ getfeed.php
@@ -3,6 +3,7 @@
   //https://github.com/J7mbo/twitter-api-php
   require_once("TwitterAPIExchange.php");
   require_once("config.php");
+  $file_name = "cache.txt";
   $url = "https://api.twitter.com/1.1/search/tweets.json";
   $getField = "?q=%23Paris&result_type=recent";
   $requestMethod = "GET";
@@ -12,6 +13,13 @@
                          ->buildOauth($url, $requestMethod)
                          ->performRequest();
 
-  echo $dataFromAPI;
+  //var_dump(json_decode($dataFromAPI)->statuses);
+  $object = new stdClass();
+  $object->date = date("c");
+  $object->statuses = json_decode($dataFromAPI)->statuses;
+
+  file_put_contents($file_name, json_encode($object));
+
+  echo json_encode($object);
 
 ?>
